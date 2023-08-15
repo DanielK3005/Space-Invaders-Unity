b5beeb74d3b09df4765fc4f4000617f2300ec7cc
Dor Edelman <doredelman@gmail.com>
2023-08-15 11:52:01 +0300

    firing logic complete

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,15 +1,31 @@
 using UnityEngine;
 using DG.Tweening;
+using Random = UnityEngine.Random;
 
 public class Shooter : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private float projectileSpeed = 10f;
     [SerializeField] private float projectileLifeTime = 5f;
-    [SerializeField] private float firingRate = 0.2f;
+    [SerializeField] private float baseFiringRate = 0.2f;
+    
+    [Header("AI")]
+    [SerializeField] private bool useAI;
+    [SerializeField] private float firingRateVariance = 0f;
+    [SerializeField] private float minimumFiringRate = 0.1f;
 
-    public bool isFiring;
+    [HideInInspector] public bool isFiring;
     private Sequence _firingSequence;
+
+    private void Start()
+    {
+        if (useAI)  //the shooter is a enemy made in EnemySpawner
+        {
+            isFiring = true;
+            baseFiringRate = Mathf.Max(baseFiringRate - firingRateVariance, minimumFiringRate);
+        }
+    }
 
     private void Update()
     {
@@ -20,6 +36,7 @@ public class Shooter : MonoBehaviour
     {
         if (isFiring && _firingSequence == null)
         {
+            float firingRate = useAI ? baseFiringRate + Random.Range(-firingRateVariance, firingRateVariance) : baseFiringRate;
             _firingSequence = DOTween.Sequence()
                 .AppendCallback(FireProjectile) // Fire the first projectile immediately
                 .AppendInterval(firingRate) // Initial delay
@@ -36,12 +53,22 @@ public class Shooter : MonoBehaviour
 
     private void FireProjectile()
     {
+        Vector3 shootingDirection = useAI ? -transform.up : transform.up; 
         GameObject instance = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
         Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
         if (rb != null)
         {
-            rb.velocity = transform.up * projectileSpeed;
+            rb.velocity = shootingDirection * projectileSpeed;
         }
         Destroy(instance, projectileLifeTime);
+    }
+    
+    private void OnDestroy()
+    {
+        if (_firingSequence != null)
+        {
+            _firingSequence.Kill();
+            _firingSequence = null;
+        }
     }
 }
